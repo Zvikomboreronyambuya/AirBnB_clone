1320b42a72a3d9a69cc222314aa0d5770c070cd0
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-12-07 01:01:50 -0800

    The Console

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Unittest module for the User Class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.user import User
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+
+    """Test Cases for the User class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of User class."""
+
+        a = User()
+        self.assertEqual(str(type(a)), "<class 'models.user.User'>")
+        self.assertIsInstance(a, User)
+        self.assertTrue(issubclass(type(a), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of User class."""
+        attributes = storage.attributes()["User"]
+        o = User()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
